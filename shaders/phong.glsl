2a2475b1682ff542aec602f9c0d438d483fbe359
Patrick Pelletier <mitthrawn@hotmail.com>
2019-09-28 20:33:02 -0400

    minor fix

--- shaders/phong.glsl
+++ shaders/phong.glsl
@@ -61,11 +61,13 @@ void main()
     vec4 textureDiffuse = texture(uniformTextureDiffuse, inData.texcoord);
     vec4 textureSpecular = texture(uniformTextureSpecular, inData.texcoord);
     vec4 textureNormal = texture(uniformTextureNormal, inData.texcoord);
+    vec4 textureOcclusion = texture(uniformTextureOcclusion, inData.texcoord);
 
     // discard textureNormal when ~= (0,0,0)
     vec3 normal = length(textureNormal) > 0.1 ? inData.TBN * normalize(2.0 * vec3(textureNormal.x, 1.0 - textureNormal.y, textureNormal.z) - 1.0) : inData.normal;
     vec3 viewDir = normalize(uniformData.cameraPosition - inData.vertex);
     vec3 lightDir = normalize(uniformData.lightPosition - inData.vertex);
+    vec3 occlusion = mix(vec3(1), textureOcclusion.rgb, textureOcclusion.a);
 
     const float kPi = 3.14159265;
     const float kShininess = 16.0;
@@ -77,8 +79,7 @@ void main()
     float diffuseFactor = dot(normal, lightDir);
     if (diffuseFactor > 0.0)
     {
-        vec4 textureOcclusion = texture(uniformTextureOcclusion, inData.texcoord);
-        fragment += vec4(vec3(diffuseFactor) * (textureOcclusion.rgb/1.5+(1-1/1.5)) * (textureDiffuse.rgb + spec * textureSpecular.rgb * textureOcclusion.rgb), 0.0);
+        fragment += vec4(vec3(diffuseFactor) * (occlusion/1.5+vec3(1-1/1.5)) * (textureDiffuse.rgb + spec * textureSpecular.rgb * occlusion), 0.0);
     }
 }
 #endif
